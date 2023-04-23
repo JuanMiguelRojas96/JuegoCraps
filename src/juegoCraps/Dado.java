16e91d12da64ecfca1c6e288eca7b1ac0546df01
JuanMiguel <juan.noriega@correounivalle.edu.co>
2023-04-23 11:47:23 -0500

    Agregando comentarios

--- src/juegoCraps/Dado.java
+++ src/juegoCraps/Dado.java
@@ -5,6 +5,10 @@ import java.util.Random;
 public class Dado {
   private int cara;
 
+  /**
+   * getCara returns a number between 1 and 6
+   * @return
+   */
   public int getCara() {
     Random aleatorio = new Random();
     cara = aleatorio.nextInt(6)+1;
